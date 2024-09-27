aaf0aa90b0a236d668b18023456763ac60b994e5
yosef <yosefisraelgoren@gmail.com>
2024-09-27 11:36:21 +0300

    moved glfw to dedicated folder

--- GlfwDisplay/frame_cli.py
+++ GlfwDisplay/frame_cli.py
@@ -2,10 +2,10 @@ import socket as sc
 import time
 import random
 
-PEER = ('localhost', 8080)
+PEER = ('172.26.112.1', 8080)
 
 s = sc.socket(sc.AF_INET, sc.SOCK_DGRAM)
 while True:
-	payload = bytes([random.randint(0, 255) for i in range(1200)])
-	time.sleep(0.005)
+	payload = bytes([random.randint(0, 255) for i in range(1440)])
+	time.sleep(0.04)
 	print(s.sendto(payload, PEER))
--- GlfwDisplay/frames.hpp
+++ GlfwDisplay/frames.hpp
@@ -2,10 +2,10 @@
 
 #include <stdint.h>
 
-#define CELL_PIXELS 64
+#define CELL_PIXELS 32
 #define WIDTH_HOST (WIDTH_GUEST * CELL_PIXELS)
 #define HEIGHT_HOST (HEIGHT_GUEST * CELL_PIXELS)
-#define WIDTH_GUEST 20
+#define WIDTH_GUEST 24
 #define HEIGHT_GUEST 20
 // #define WIDTH_GUEST 800
 // #define HEIGHT_GUEST 600
